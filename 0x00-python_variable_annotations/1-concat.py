e47487a20c1726e5cd5e58999a5722f7d43105df
JamesWainaina <jamesgatheru9905@gmail.com>
2024-06-09 14:15:51 +0300

    pycodestyle task 0

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+def concat(str1: str, str2: str) -> str:
+"""function for concatinating two strings"""
+	return str1 + str2
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+import math
+
+def floor(n: float) -> float:
+	Math.floor(n)
