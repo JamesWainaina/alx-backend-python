8bebc1f971945a5337689be33872c9bda7dbd1ff
JamesWainaina <jamesgatheru9905@gmail.com>
2024-06-09 14:31:39 +0300

    task 1 function for concatinating strings

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""function for concatinating two strings"""
+
 
 def concat(str1: str, str2: str) -> str:
-"""function for concatinating two strings"""
-	return str1 + str2
+    """
+    function for concatinating two strings
+    """
+    return str1 + str2
