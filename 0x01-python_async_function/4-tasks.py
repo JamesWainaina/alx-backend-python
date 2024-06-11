5bd6b8f476a392b12bd17782d1c486b601334171
JamesWainaina <jamesgatheru9905@gmail.com>
2024-06-11 14:12:58 +0300

    pycodeStyle fix

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -7,6 +7,7 @@ from typing import List
 
 task_wait_random = __import__('3-tasks').task_wait_random
 
+
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
     function similar to task_n but task_wait_random is being called
@@ -18,7 +19,7 @@ async def task_wait_n(n: int, max_delay: int) -> List[float]:
     for _ in range(n):
         task = task_wait_random(max_delay)
         tasks.append(task)
-    
+
     for task in asyncio.as_completed(tasks):
         delay = await task
         delays.append(delay)
