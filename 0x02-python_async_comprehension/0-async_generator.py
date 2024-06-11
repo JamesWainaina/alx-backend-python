0e3fdb318737884fab71aa7a62d5705d77fb449c
JamesWainaina <jamesgatheru9905@gmail.com>
2024-06-11 15:17:01 +0300

    use of Generator function

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,13 +2,13 @@
 """Async Generator"""
 import random
 import asyncio
-from typing import List
-
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
-    """Async Generator"""
+async def async_generator() -> Generator[float, None, None]:
+    """
+    Async generator
+    """
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
