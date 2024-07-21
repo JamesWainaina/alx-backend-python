a7bf970d96f8e8430501bf0030f3eef36b653881
JamesWainaina <jamesgatheru9905@gmail.com>
2024-07-21 15:26:03 +0300

    task 2 returning json response from HTTP calls using Mock() function

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -6,8 +6,9 @@ Parametizing a unit test using utils.access_nested_map function
 
 import unittest
 from typing import Any, Mapping, Sequence
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 from parameterized import parameterized
+from unittest.mock import patch, Mock
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -41,3 +42,21 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """
+    Test get_json function
+    """
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    def test_get_json(self, test_url: str, test_payload: Mapping) -> Any:
+        """
+        Test method for get_json function
+        """
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+        with patch("requests.get", return_value=mock_response):
+            self.assertEqual(get_json(test_url), test_payload)
