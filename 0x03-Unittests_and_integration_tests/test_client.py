d26ca1e833b6f327ffbe3ace9eb9418117574b0c
JamesWainaina <jamesgatheru9905@gmail.com>
2024-07-21 16:32:10 +0300

    task 5 using mock property to tetst memoization

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -34,3 +34,24 @@ class TestGithubOrgClient(unittest.TestCase):
         mock_get_json.assert_called_once_with(
             "https://api.github.com/orgs/{}".format(org_name)
         )
+
+    def test_public_repos_url(self) -> None:
+        """
+        Test the public_repos_url method
+        """
+        with patch("client.GithubOrgClient.org", new_callable=PropertyMock) as mock_org:
+            mock_org.return_value = {
+                "repos_url": "https://api.github.com/users/google/repos",
+            }
+
+            # Arrange
+            org_name: str = "google"
+            client = GithubOrgClient(org_name)
+
+            # Act
+            result = client._public_repos_url
+
+            # Assert
+            self.assertEqual(
+                result, "https://api.github.com/users/google/repos"
+            )
